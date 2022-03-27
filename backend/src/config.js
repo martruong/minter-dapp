b42fe07bea3bb682dcee067006e71711cce52925
martruong <mark.truong27@gmail.com>
2022-03-27 18:41:13 +1100

    generic false

--- backend/src/config.js
+++ backend/src/config.js
@@ -75,7 +75,7 @@ const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
-const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
+const GENERIC = false; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
 const GENERIC_TITLE = CONTRACT_NAME; // Replace with what you want the generic titles to say if you want it to be different from the contract name.
 const GENERIC_DESCRIPTION = "Which Llama will you get?"; // Replace with what you want the generic descriptions to say.
 const GENERIC_IMAGE = "https://ipfs.io/ipfs/bafkreidbonyt3qzdrc2xqufhaneue6fvnw2mpqgmfcs5z25zof2mgczywa"; // Replace with your generic image that will display for all NFTs pre-reveal.
